ed09acbf5c35731c60be0dd64d07d73da40f9c55
Sumisi <oic.gp.shiori.sumi@gmail.com>
2021-12-21 16:14:05 +0900

    bullet

---
+++ Animal/Assets/Scripts/bullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    //public Enemy targetEnemy;
+    //private float speed = 10;
+    //private GameObject target;
+
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+
+            //このオブジェクトを破壊する
+            Destroy(gameObject);
+
+        }
+    }
+}
