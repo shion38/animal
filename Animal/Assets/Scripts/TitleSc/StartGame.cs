59bd084dc2bdc9c60752a3c2b55f1b58bf66be6c
MSI\mk06m <oic.gp.masakazu.sakiya@gmail.com>
2021-12-28 14:13:33 +0900

    タイトルにＢＧＭと調整用のスライダーつけました

--- Animal/Assets/Scripts/TitleSc/StartGame.cs
+++ Animal/Assets/Scripts/TitleSc/StartGame.cs
@@ -7,6 +7,6 @@ public class StartGame : MonoBehaviour
 {
     public void LoadingNewScene()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("SampleScene");//メインシーンへ
     }
 }
