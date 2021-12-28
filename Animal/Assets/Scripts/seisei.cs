851db5d770b4b40be21e92ee0908b90fa7091bd5
shota11 <kawa.220611@gmail.com>
2021-12-28 14:08:48 +0900

    スクリプトを置き換える！！！！

---
+++ Animal/Assets/Scripts/seisei.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class seisei : MonoBehaviour
+{
+    GameObject clickedGameObject;
+
+    SpriteRenderer MainSpriteRenderer;
+    public Sprite HoldSprite;
+
+    void Start()
+    {
+        // このobjectのSpriteRendererを取得
+        MainSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            clickedGameObject = null;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit2d = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction);
+
+            if (hit2d)
+            {
+                clickedGameObject = hit2d.transform.gameObject;
+            }
+
+            MainSpriteRenderer.sprite = HoldSprite;
+        }
+
+    }
+
+}
+
