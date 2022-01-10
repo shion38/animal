ee49f431e57184f6934ce59ba4b3c5fc1149c766
MSI\mk06m <oic.gp.masakazu.sakiya@gmail.com>
2022-01-10 13:37:45 +0900

    クリア画面で成功と失敗の判別と捕まえた数の表示を足しました

---
+++ Animal/Assets/Scripts/EndSc/SuccessCon.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SuccessCon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*
+        if (  == 0) //ここに残りライフを入れる
+        {
+            SuccessPanel.SetActive(false);
+            FailurePanel.SetActive(true);
+        }
+        else
+        {
+            SuccessPanel.SetActive(true);
+            FailurePanel.SetActive(false);
+        }
+
+        GetText.GetComponent<Text>().text = "つかまえたかず\n" + Score.ToString("D4"); //ここに捕まえた数を入れる
+        */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
